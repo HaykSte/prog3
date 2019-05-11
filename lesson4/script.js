c3a4a824d847168714c952733269938ea6ffa2ef
Aram Gumrikyan <aram.gumrikyan.01@gmail.com>
2019-05-11 14:02:05 +0400

    lesson4

---
+++ lesson4/script.js
@@ -0,0 +1,55 @@
+// var clickCount = 0;
+// function clickHandler(evt){
+//    clickCount++;
+//    console.log(evt);
+//    var str = "Thanks for clicking " + clickCount;
+//    this.innerText = str;
+// }
+
+// var p = document.getElementById("pElement");
+// p.addEventListener("click", clickHandler);
+
+// window.onclick = bodyClick;
+
+// function windowLoad() {
+//     console.log("Window is loaded");
+//  }
+//  window.onload = windowLoad;
+
+
+// function setup()
+// {
+
+// }
+
+// function preload() {
+//     console.log("Window is loaded");
+//  }
+
+
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;
+
